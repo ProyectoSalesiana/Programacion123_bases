71b58fe287003eea371cc8416381518c280f6936
chaxz <31754229+chaxz@users.noreply.github.com>
2017-09-08 00:04:27 -0500

    Primera prueba del commit

---
+++ programacion3/src/bases/conexion2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bases;
+
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import java.sql.Connection;
+
+/**
+ *
+ * @author DC
+ */
+public class conexion2 {
+
+    public static String mensajeError = null;
+
+    public static Connection con() {
+        Connection con = null;
+        try {
+            SQLServerDataSource ds = new SQLServerDataSource();
+            ds.setUser("sa");
+            ds.setPassword("sa.1");
+            ds.setServerName("localhost");
+            ds.setPortNumber(1438);
+            ds.setDatabaseName("ejemplo");
+            con = ds.getConnection();
+        } catch (Exception e) {
+            mensajeError = e.getMessage();
+        }
+        return con;
+    }
+}
---
+++ programacion3/src/bases/ejecutarconexion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bases;
+
+import java.sql.Connection;
+
+
+/**
+ *
+ * @author DC
+ */
+public class ejecutarconexion {
+
+    public static void main(String[] args) {
+        Connection con = conexion2.con();
+        if(con!=null)
+        {
+            System.out.println("Conectado Correctamente");}
+        else{
+            System.out.println("Error: "+ conexion2.mensajeError);}
+    }
+
+}
